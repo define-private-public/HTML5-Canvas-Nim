8dc53a2beee9da4bc4f3e76eb1ddb291a620e5f4
Benjamin N. Summerton <def.pri.pub@gmail.com>
2017-01-10 20:57:32 -0500

    Added a note

--- html5_canvas.nim
+++ html5_canvas.nim
@@ -1,5 +1,6 @@
-# TODO header
-
+# This file is simply a meta-file that combines the necessary parts for this
+# package to work.  Though because of Circular import issues, two procs are
+# placed here.
 
 when not defined(js) and not defined(Nimdoc):
   {.error: "This module only works for the JavaScript target.".}
